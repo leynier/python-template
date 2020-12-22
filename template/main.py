549f5ef26c7e6ce78c39079bc4db9ba2e5ef975d
leynier <leynier41@gmail.com>
2020-12-21 23:03:23 -0500

    Implement API versioning for FastAPI

--- template/main.py
+++ template/main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
+from os.path import join
 
-app = FastAPI()
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
+
+from .api.main import api
+
+app = FastAPI(docs_url=None, redoc_url=None)
+
+app.mount("/api", api)
 
 
-@app.get("/")
-def run():
-    return {"message": "Hello World!"}
+@app.get("/", include_in_schema=False)
+def index(request: Request):
+    return RedirectResponse(join(request.url.path, "api", ""))
